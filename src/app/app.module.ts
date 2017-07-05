3e225cbbd6930ca2b53d0020a2fa79df6a7a25bd
unknown <robertofayella@gmail.com>
2017-07-05 01:46:20 +0100

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,20 +15,11 @@ import { LoginComponent }           from './components/login/app.login';
 import { RegisterComponent }        from './components/register/app.register';
 import { CampainsComponent }        from './components/campains/app.campains';
 import { HomeComponent }            from './components/home/app.home';
-<<<<<<< HEAD
 import { ShowCharacters }           from './components/app.showCharacters';
 import { InputNumberComponent }     from './components/inputs/input-number';
 import { DiceInputComponent }       from './components/inputs/dice-input';
 import { RollFilterPipe }           from './utilities/roll-filter-pipe';
-=======
 
-import { ShowCharacters }           from './components/app.showCharacters';
-
-import { InputNumberComponent }     from './components/inputs/input-number';
-import { DiceInputComponent }       from './components/inputs/dice-input';
-import { RollFilterPipe }           from './utilities/roll-filter-pipe';
-
->>>>>>> 86d4eede9d57ec2ce922d04d8150c098f86ee499
 
 //SERVICES
 import { Utilities }                from './utilities/app.utilities';
