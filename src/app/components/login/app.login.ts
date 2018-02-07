ddc3ac53e2cae6dc418b2b4efee704f251111cab
Prichmondo <riccardo.volpato@gmail.com>
2018-02-07 21:40:24 +0000

    added bootstrap

--- src/app/components/login/app.login.ts
+++ src/app/components/login/app.login.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../../services/authentication.service';
 import { Broadcaster } from '../../services/broadcast';
 
 @Component({
   selector: 'login',
-  templateUrl: './app.login.html'
+  templateUrl: './app.login.html',
+  styleUrls: ['./app.login.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
   
